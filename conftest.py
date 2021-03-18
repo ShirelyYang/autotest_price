491f7123a20da2249934506e92544f0069102daf
ShirelyYang <yangcx0123@126.com>
2021-03-18 09:52:00 +0800

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\app.py")))
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\my\my.py")))
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\my\toolbox\toolbox_more.py")))
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\found\found.py")))
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\found\headlines\headlines.py")))
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\found\small_video\small_video.py")))
+sys.path.append(os.path.dirname(os.path.abspath(r"D:\autotest_price_git\autotest_price\price_page\market\market.py")))
+
